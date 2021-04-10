92654271e91f95a406e1f21ffa307ff8e7066bf1
MaulingMonkey <git@maulingmonkey.com>
2021-04-09 21:30:47 -0700

    fix !windows builds

--- examples/message_loop_only.rs
+++ examples/message_loop_only.rs
@@ -1,8 +1,10 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use kakistocracy::windows::*;
+#[cfg(not(windows))] fn main() {}
 
-fn main() {
+#[cfg(windows)] fn main() {
+    use kakistocracy::windows::*;
+
     post_quit_message(-42);
     let exit = message_loop_until_wm_quit();
     assert_eq!(exit, -42);
