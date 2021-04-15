769d33618f08848dc7e6c1770a522a721b81997e
MaulingMonkey <git@maulingmonkey.com>
2021-04-14 17:20:05 -0700

    RectExt: document

--- src/windows/rect.rs
+++ src/windows/rect.rs
@@ -1,40 +1,56 @@
+use winapi::shared::ntdef::*;
 use winapi::shared::windef::RECT;
 
 use std::ops::Range;
 
 
 
+/// Extension methods for [`RECT`].
 pub trait RectExt {
+    /// [`LONG`] for [`RECT`].  A component of a position in the coordinate space.  Likely signed.
     type Pos;
+
+    /// `left` .. `right`
     fn xrange(&self) -> Range<Self::Pos>;
+
+    /// `top` .. `bottom`
     fn yrange(&self) -> Range<Self::Pos>;
 
+    /// [`ULONG`] for [`RECT`].  A component of a size that should cover the entire coordinate space.  Unsigned.
     type Size;
+
+    /// `right` - `left` (or `0` if `right` < `left`)
     fn width(&self) -> Self::Size;
+
+    /// `bottom` - `top` (or `0` if `bottom` < `top`)
     fn height(&self) -> Self::Size;
+
+    /// `(width(), height())`
     fn size(&self) -> (Self::Size, Self::Size) { (self.width(), self.height()) }
 }
 
+
+
 impl RectExt for RECT {
-    type Pos = i32;
-    fn xrange(&self) -> Range<i32> { self.left .. self.right }
-    fn yrange(&self) -> Range<i32> { self.top .. self.bottom }
+    type Pos = LONG;
+    fn xrange(&self) -> Range<LONG> { self.left .. self.right }
+    fn yrange(&self) -> Range<LONG> { self.top .. self.bottom }
 
-    type Size = u32;
+    type Size = ULONG;
 
-    fn width(&self) -> u32 {
+    fn width(&self) -> ULONG {
         if self.left > self.right {
             0
         } else {
-            self.right.wrapping_sub(self.left) as u32
+            self.right.wrapping_sub(self.left) as ULONG
         }
     }
 
-    fn height(&self) -> u32 {
+    fn height(&self) -> ULONG {
         if self.top > self.bottom {
             0
         } else {
-            self.bottom.wrapping_sub(self.top) as u32
+            self.bottom.wrapping_sub(self.top) as ULONG
         }
     }
 }
@@ -49,7 +65,7 @@ impl IntoRect for RECT {
     fn into(self) -> RECT { self }
 }
 
-impl IntoRect for Range<(i32, i32)> {
+impl IntoRect for Range<(LONG, LONG)> {
     fn into(self) -> RECT { RECT {
         left:   self.start.0,
         top:    self.start.1,
@@ -58,7 +74,7 @@ impl IntoRect for Range<(i32, i32)> {
     }}
 }
 
-impl IntoRect for Range<[i32; 2]> {
+impl IntoRect for Range<[LONG; 2]> {
     fn into(self) -> RECT { RECT {
         left:   self.start[0],
         top:    self.start[1],
@@ -67,7 +83,7 @@ impl IntoRect for Range<[i32; 2]> {
     }}
 }
 
-impl IntoRect for (Range<i32>, Range<i32>) {
+impl IntoRect for (Range<LONG>, Range<LONG>) {
     fn into(self) -> RECT { RECT {
         left:   self.0.start,
         top:    self.1.start,
@@ -76,7 +92,7 @@ impl IntoRect for (Range<i32>, Range<i32>) {
     }}
 }
 
-impl IntoRect for [Range<i32>; 2] {
+impl IntoRect for [Range<LONG>; 2] {
     fn into(self) -> RECT { RECT {
         left:   self[0].start,
         top:    self[1].start,
