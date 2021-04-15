bf9323df8db7226c94a61072b46b349983505622
MaulingMonkey <git@maulingmonkey.com>
2021-04-14 17:11:15 -0700

    window::prelude

--- src/windows/_windows.rs
+++ src/windows/_windows.rs
@@ -7,6 +7,8 @@ pub use ::winapi;
 
 #[path = "d3d9/_d3d9.rs"] pub mod d3d9;
 
+pub mod prelude;
+
 mod errors;         pub use errors::*;
 mod message_loop;   pub use message_loop::*;
 mod misc;           pub use misc::*;
---
+++ src/windows/prelude.rs
@@ -0,0 +1,16 @@
+//! A prelude for conveniently writing platform-specific code.
+//!
+//! Includes extension traits, and some important crates.
+
+
+
+// crates
+
+pub use ::mcom;
+pub use ::winapi;
+
+
+
+// extension traits
+
+#[doc(no_inline)] pub use super::RectExt;
