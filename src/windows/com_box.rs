1d0c1c77be585b20119f9eabfe670a81732e0365
MaulingMonkey <git@maulingmonkey.com>
2021-04-17 21:37:57 -0700

    ComBox -> UnkWrap{,Rc,Weak}

--- src/windows/com_box.rs
+++ src/windows/com_box.rs
@@ -1,6 +1,6 @@
 #![allow(dead_code)] // XXX
 
-use crate::windows::type_guid;
+use crate::windows::{Error, type_guid};
 
 use winapi::Interface;
 use winapi::ctypes::c_void;
@@ -11,11 +11,14 @@ use winapi::um::unknwnbase::{IUnknown, IUnknownVtbl};
 
 use std::any::*;
 use std::borrow::Borrow;
+use std::cmp::Ordering;
 use std::convert::*;
 use std::fmt::{self, Debug, Display, Formatter};
+use std::hash::{Hash, Hasher};
 use std::mem::forget;
+use std::ops::*;
 use std::ptr::*;
-use std::sync::Arc;
+use std::sync::{Arc, Weak};
 
 // TODO:
 // - [ ] move into mcom?
@@ -23,28 +26,120 @@ use std::sync::Arc;
 
 
 
-#[repr(C)] pub struct ComBox<T: Any> {
+pub(crate) struct UnkWrapRc  <T: Any>(Arc <UnkWrap<T>>);
+pub(crate) struct UnkWrapWeak<T: Any>(Weak<UnkWrap<T>>);
+
+impl<T: Any> UnkWrapRc<T> {
+    pub fn new(value: T) -> Self { Self(Arc::new(UnkWrap::new(value))) }
+    // try_unwrap
+    // into_raw
+    // as_ptr
+    // from_raw
+    pub fn downgrade(this: &Self) -> UnkWrapWeak<T> { UnkWrapWeak(Arc::downgrade(&this.0)) }
+    pub fn weak_count(this: &Self) -> usize { Arc::weak_count(&this.0) }
+    pub fn strong_count(this: &Self) -> usize { Arc::strong_count(&this.0) }
+    // increment_strong_count
+    // decrement_strong_count
+    pub fn ptr_eq(this: &Self, other: &Self) -> bool { Arc::ptr_eq(&this.0, &other.0) }
+    // make_mut
+    // get_mut
+    // get_mut_unchecked
+    // downcast
+}
+
+impl<T: Any> UnkWrapWeak<T> {
+    pub fn new() -> Self { Self(Weak::new()) }
+    // as_ptr
+    // into_raw
+    // from_raw
+    pub fn upgrade(&self) -> Option<UnkWrapRc<T>> { self.0.upgrade().map(|wrap| UnkWrapRc(wrap)) }
+    pub fn strong_count(&self) -> usize { self.0.strong_count() }
+    pub fn weak_count(&self) -> usize { self.0.weak_count() }
+    pub fn ptr_eq(&self, other: &Self) -> bool { self.0.ptr_eq(&other.0) }
+}
+
+unsafe impl<T: Any + Send> Send for UnkWrapRc<T> {}
+unsafe impl<T: Any + Sync> Sync for UnkWrapRc<T> {}
+unsafe impl<T: Any + Send> Send for UnkWrapWeak<T> {}
+unsafe impl<T: Any + Sync> Sync for UnkWrapWeak<T> {}
+
+impl<T: Any> UnkWrapRc<T> {
+    pub(crate) fn from_com_unknown(unk: &mcom::Rc<IUnknown>) -> Option<Self> { Self::try_from(unk).ok() }
+    pub(crate) fn to_com_unknown(&self) -> mcom::Rc<IUnknown> { mcom::Rc::<IUnknown>::from(self.clone()) }
+}
+
+impl<T: Any> UnkWrapWeak<T> {
+    // conversion?
+}
+
+impl<T: Any> AsRef<T>   for UnkWrapRc<T> { fn as_ref(&self) -> &T { &self.0.as_inner() } }
+impl<T: Any> Borrow<T>  for UnkWrapRc<T> { fn borrow(&self) -> &T { &self.0.as_inner() } }
+impl<T: Any> Deref      for UnkWrapRc<T> { fn deref(&self)  -> &T { &self.0.as_inner() } type Target = T; }
+impl<T: Any> Clone      for UnkWrapRc<T> { fn clone(&self) -> Self { Self(self.0.clone()) } }
+
+impl<T: Any> Clone      for UnkWrapWeak<T> { fn clone(&self) -> Self { Self(self.0.clone()) } }
+
+impl<T: Any + Default   > Default       for UnkWrapRc<T> { fn default() -> Self { Self::new(T::default()) } }
+impl<T: Any + Debug     > Debug         for UnkWrapRc<T> { fn fmt(&self, fmt: &mut Formatter<'_>) -> fmt::Result { Debug  ::fmt(&self.0.as_inner(), fmt) } }
+impl<T: Any + Display   > Display       for UnkWrapRc<T> { fn fmt(&self, fmt: &mut Formatter<'_>) -> fmt::Result { Display::fmt(&self.0.as_inner(), fmt) } }
+impl<T: Any + PartialEq > PartialEq     for UnkWrapRc<T> { fn eq(&self, other: &Self) -> bool { self.0.as_inner().eq(other.0.as_inner()) } }
+impl<T: Any + Eq        > Eq            for UnkWrapRc<T> {}
+impl<T: Any + PartialOrd> PartialOrd    for UnkWrapRc<T> { fn partial_cmp(&self, other: &Self) -> Option<Ordering> { self.0.as_inner().partial_cmp(other.0.as_inner()) } }
+impl<T: Any + Ord       > Ord           for UnkWrapRc<T> { fn cmp(&self, other: &Self) -> Ordering { self.0.as_inner().cmp(other.0.as_inner()) } }
+impl<T: Any + Hash      > Hash          for UnkWrapRc<T> { fn hash<H: Hasher>(&self, state: &mut H) { self.0.as_inner().hash(state) } }
+
+impl<T: Any             > Default       for UnkWrapWeak<T> { fn default() -> Self { Self::new() } }
+impl<T: Any + Debug     > Debug         for UnkWrapWeak<T> { fn fmt(&self, fmt: &mut Formatter<'_>) -> fmt::Result { match self.upgrade() {
+    None    => fmt.debug_tuple("UnkWrapWeak").field(&format_args!("None")).finish(),
+    Some(c) => fmt.debug_tuple("UnkWrapWeak").field(&Some(&*c)).finish(),
+}}}
+
+// Not (yet?) implemented:
+// - Unpin, Pointer, UnwindSafe, CoerceUnsized, DispatchFromDyn
+
+// Do not implement:
+// - AsMut      (mutable access when COM objects are always refcounted / typically shared)
+// - BorrowMut  (mutable access when COM objects are always refcounted / typically shared)
+// - DerefMut   (mutable access when COM objects are always refcounted / typically shared)
+
+impl<T: Any> TryFrom<&mcom::Rc<IUnknown>> for UnkWrapRc<T> {
+    type Error = Error;
+    fn try_from(value: &mcom::Rc<IUnknown>) -> Result<Self, Self::Error> {
+        let value = value.try_cast::<UnkWrap<T>>().ok_or(Error::new_hr("IUnknown::QueryInterface", E_NOINTERFACE, ""))?;
+        let value = unsafe { Arc::from_raw(value.into_raw()) };
+        Ok(Self(value))
+    }
+}
+
+impl<T: Any> From<UnkWrapRc<T>> for mcom::Rc<IUnknown> {
+    fn from(value: UnkWrapRc<T>) -> Self {
+        let raw : *const UnkWrap<T> = Arc::into_raw(value.0);
+        let raw = raw as *mut UnkWrap<T> as *mut IUnknown;
+        unsafe { mcom::Rc::from_raw_unchecked(raw) }
+    }
+}
+
+
+
+#[repr(C)] pub(crate) struct UnkWrap<T: Any> {
     vtable: *const IUnknownVtbl,
     value:  T,
 }
 
-impl<T: Any> ComBox<T> {
-    pub fn new(value: T) -> mcom::Rc<Self> {
+impl<T: Any> UnkWrap<T> {
+    fn new(value: T) -> Self {
         let vtable : &'static IUnknownVtbl = &IUnknownVtbl {
             AddRef:         Self::add_ref,
             Release:        Self::release,
             QueryInterface: Self::query_interface,
         };
-        let cb = Arc::new(Self {
+        Self {
             vtable: vtable,
             value,
-        });
-        let cb : *const Self = Arc::into_raw(cb);
-        let cb : *mut   Self = cb as *mut _;
-        unsafe { mcom::Rc::from_raw_unchecked(cb) }
+        }
     }
 
-    pub fn as_inner(&self) -> &T { &self.value }
+    fn as_inner(&self) -> &T { &self.value }
 
     unsafe extern "system" fn add_ref(this: *mut IUnknown) -> ULONG {
         // https://docs.microsoft.com/en-us/windows/win32/api/unknwn/nf-unknwn-iunknown-addref
@@ -78,30 +173,7 @@ impl<T: Any> ComBox<T> {
     }
 }
 
-unsafe impl<T: Any + Send> Send for ComBox<T> {}
-unsafe impl<T: Any + Sync> Sync for ComBox<T> {}
+unsafe impl<T: Any + Send> Send for UnkWrap<T> {}
+unsafe impl<T: Any + Sync> Sync for UnkWrap<T> {}
 
-impl<T: Any> Interface  for ComBox<T> { fn uuidof() -> GUID { type_guid::<Self>() } }
-impl<T: Any> AsRef<T>   for ComBox<T> { fn as_ref(&self) -> &T { &self.value } }
-impl<T: Any> Borrow<T>  for ComBox<T> { fn borrow(&self) -> &T { &self.value } }
-// XXX: Should this deref to T (convenient), or to IUnknown (consistent, might be required for future COM pointers)
-//impl<T: Any> Deref      for ComBox<T> { fn deref(&self)  -> &IUnknown { unsafe { std::mem::transmute(self) } } type Target = IUnknown; }
-//impl<T: Any> Deref      for ComBox<T> { fn deref(&self)  -> &T { &self.value } type Target = T; }
-
-impl<T: Any + Debug  > Debug   for ComBox<T> { fn fmt(&self, fmt: &mut Formatter<'_>) -> fmt::Result { Debug  ::fmt(&self.value, fmt) } }
-impl<T: Any + Display> Display for ComBox<T> { fn fmt(&self, fmt: &mut Formatter<'_>) -> fmt::Result { Display::fmt(&self.value, fmt) } }
-
-// Not (yet?) implemented:
-// - [Partial]Eq
-// - [Partial]Ord
-// - Hash
-// - From/Into
-// - Unpin, Pointer, UnwindSafe, CoerceUnsized, DispatchFromDyn
-
-// Cannot implement:
-// - Default for mcom::Rc<ComBox<T: Default>>
-
-// Do not implement:
-// - AsMut      (mutable access when COM objects are always refcounted / typically shared)
-// - BorrowMut  (mutable access when COM objects are always refcounted / typically shared)
-// - DerefMut   (mutable access when COM objects are always refcounted / typically shared)
+impl<T: Any> Interface  for UnkWrap<T> { fn uuidof() -> GUID { type_guid::<Self>() } }
