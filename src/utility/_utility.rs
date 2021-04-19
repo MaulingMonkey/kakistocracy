edfcca9d388345bbe68e5477f2bbfa5cde82d24a
MaulingMonkey <git@maulingmonkey.com>
2021-04-18 23:38:19 -0700

    document mod utility

--- src/utility/_utility.rs
+++ src/utility/_utility.rs
@@ -1,3 +1,5 @@
+//! Misc. utility types and functions
+
 mod frame_rate_counter;         pub use frame_rate_counter::*;
 mod send_sync_cell;             pub(crate) use send_sync_cell::*;
 mod static_bytes_ref;           pub(crate) use static_bytes_ref::*;
